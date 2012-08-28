1c9bbf05a369b1eaf14b5becf30816b19b557b60
NTTdocomo <openstack-contributor-ml@nttdocomo.com>
2012-08-28 14:55:31 +0900

    test

--- nova/tests/baremetal/test_driver.py
+++ nova/tests/baremetal/test_driver.py
@@ -15,7 +15,7 @@
 #    under the License.
 
 """
-Tests for baremetal connection.
+Tests for baremetal driver.
 """
 
 import mox
@@ -28,7 +28,7 @@ from nova.tests import test_virt_drivers
 from nova.tests import utils as test_utils
 from nova.virt.baremetal import baremetal_states
 from nova.virt.baremetal import db
-from nova.virt.baremetal import driver as c
+from nova.virt.baremetal import driver as bm_driver
 from nova.virt.baremetal import volume_driver
 from nova.virt.firewall import NoopFirewallDriver
 
@@ -64,31 +64,76 @@ def class_path(class_):
     return class_.__module__ + '.' + class_.__name__
 
 
+COMMON_FLAGS = dict(
+    baremetal_sql_connection='sqlite:///:memory:',
+    baremetal_driver='nova.virt.baremetal.fake.Fake',
+    power_manager='nova.virt.baremetal.ipmi.DummyIpmi',
+    baremetal_vif_driver=class_path(FakeVifDriver),
+    firewall_driver=class_path(FakeFirewallDriver),
+    baremetal_volume_driver=class_path(FakeVolumeDriver),
+    instance_type_extra_specs=['cpu_arch:test'],
+    host=NODE['service_host'],
+)
+
+
+def _create_baremetal_stuff():
+    context = test_utils.get_test_admin_context()
+    node = db.bm_node_create(context, NODE)
+    for nic in NICS:
+        db.bm_interface_create(context,
+                               node['id'],
+                               nic['address'],
+                               nic['datapath_id'],
+                               nic['port_no'])
+    return node
+
+
+class BaremetalDriverSpawnTestCase(test.TestCase):
+
+    def setUp(self):
+        self.flags(**COMMON_FLAGS)
+        utils.clear_tables()
+        super(BaremetalDriverSpawnTestCase, self).setUp()
+        fake_image.stub_out_image_service(self.stubs)
+
+        self.context = test_utils.get_test_admin_context()
+        self.instance = test_utils.get_test_instance()
+        self.instance['uuid'] = '12345'
+        self.network_info = test_utils.get_test_network_info()
+        self.block_device_info = None
+        self.image_meta = test_utils.get_test_image_info(None, self.instance)
+        self.node = _create_baremetal_stuff()
+        self.node_id = self.node['id']
+        self.driver = bm_driver.BareMetalDriver()
+        self.kwargs = dict(
+                context=self.context,
+                instance=self.instance,
+                image_meta=self.image_meta,
+                injected_files=[('/foo', 'bar'), ('/abc', 'xyz')],
+                admin_password='testpass',
+                network_info=self.network_info,
+                block_device_info=self.block_device_info)
+
+    def tearDown(self):
+        super(BaremetalDriverSpawnTestCase, self).tearDown()
+        fake_image.FakeImageService_reset()
+
+    def test_ok(self):
+        self.driver.spawn(**self.kwargs)
+        node = db.bm_node_get(self.context, self.node_id)
+        self.assertEqual(node['instance_uuid'], self.instance['uuid'])
+        self.assertEqual(node['task_state'], baremetal_states.ACTIVE)
+
+
 class BaremetalDriverTestCase(test_virt_drivers._VirtDriverTestCase):
 
     def setUp(self):
         # Point _VirtDriverTestCase at the right module
         self.driver_module = 'nova.virt.baremetal.BareMetalDriver'
-        self.flags(baremetal_sql_connection='sqlite:///:memory:',
-                   baremetal_driver='nova.virt.baremetal.fake.Fake',
-                   power_manager='nova.virt.baremetal.ipmi.DummyIpmi',
-                   baremetal_vif_driver=class_path(FakeVifDriver),
-                   firewall_driver=class_path(FakeFirewallDriver),
-                   baremetal_volume_driver=class_path(FakeVolumeDriver),
-                   instance_type_extra_specs=['cpu_arch:test'],
-                   host=NODE['service_host'],
-                   )
-
+        self.flags(**COMMON_FLAGS)
         utils.clear_tables()
-        context = test_utils.get_test_admin_context()
-        node = db.bm_node_create(context, NODE)
-        self.node_id = node['id']
-        for nic in NICS:
-            db.bm_interface_create(context,
-                                   node['id'],
-                                   nic['address'],
-                                   nic['datapath_id'],
-                                   nic['port_no'])
+        self.node = _create_baremetal_stuff()
+        self.node_id = self.node['id']
         super(BaremetalDriverTestCase, self).setUp()
         fake_image.stub_out_image_service(self.stubs)
 
@@ -96,41 +141,28 @@ class BaremetalDriverTestCase(test_virt_drivers._VirtDriverTestCase):
         super(BaremetalDriverTestCase, self).tearDown()
         fake_image.FakeImageService_reset()
 
+    def _get_running_instance(self):
+        instance_ref = test_utils.get_test_instance()
+        network_info = test_utils.get_test_network_info()
+        image_info = test_utils.get_test_image_info(None, instance_ref)
+        self.connection.spawn(self.ctxt, instance_ref, image_info,
+                              [], 'herp', network_info=network_info)
+        return instance_ref, network_info
+
     def test_loading_baremetal_drivers(self):
         from nova.virt.baremetal import fake
-        drv = c.BareMetalDriver()
+        drv = bm_driver.BareMetalDriver()
         self.assertTrue(isinstance(drv.baremetal_nodes, fake.Fake))
         self.assertTrue(isinstance(drv._vif_driver, FakeVifDriver))
         self.assertTrue(isinstance(drv._firewall_driver, FakeFirewallDriver))
         self.assertTrue(isinstance(drv._volume_driver, FakeVolumeDriver))
 
-    def test_spawn(self):
-        context = test_utils.get_test_admin_context()
-        instance = test_utils.get_test_instance()
-        instance['uuid'] = '12345'
-        network_info = test_utils.get_test_network_info()
-        block_device_info = None
-        image_meta = test_utils.get_test_image_info(None, instance)
-
-        drv = c.BareMetalDriver()
-        drv.spawn(context,
-                  instance=instance,
-                  image_meta=image_meta,
-                  injected_files=[('/foo', 'bar'), ('/abc', 'xyz')],
-                  admin_password='testpass',
-                  network_info=network_info,
-                  block_device_info=block_device_info)
-
-        n = db.bm_node_get(context, self.node_id)
-        self.assertEqual(n['instance_uuid'], instance['uuid'])
-        self.assertEqual(n['task_state'], baremetal_states.ACTIVE)
-
     def test_get_host_stats(self):
         self.flags(instance_type_extra_specs=['cpu_arch:x86_64', 'x:123',
                                               'y:456', ])
-        drv = c.BareMetalDriver()
-        s = drv._get_host_stats()
-        es = s['instance_type_extra_specs']
+        drv = bm_driver.BareMetalDriver()
+        cap = drv.get_host_stats()
+        es = cap['instance_type_extra_specs']
         self.assertEqual(es['cpu_arch'], 'x86_64')
         self.assertEqual(es['x'], '123')
         self.assertEqual(es['y'], '456')
@@ -150,8 +182,8 @@ class BaremetalDriverTestCase(test_virt_drivers._VirtDriverTestCase):
                                       memory_mb=2000, local_gb=20)
         ns = [N1, N2, N3, N4, ]
         context = test_utils.get_test_admin_context()
-        self.stubs.Set(c, '_get_baremetal_nodes', lambda ctx: ns)
-        drv = c.BareMetalDriver()
+        self.stubs.Set(bm_driver, '_get_baremetal_nodes', lambda ctx: ns)
+        drv = bm_driver.BareMetalDriver()
 
         dic = drv._max_baremetal_resources(context)
         self.assertEqual(dic.get('vcpus'), 1)
@@ -260,10 +292,10 @@ class FindHostTestCase(test.TestCase):
         inst['vcpus'] = 1
         inst['memory_mb'] = 1024
 
-        self.mox.StubOutWithMock(c, '_get_baremetal_nodes')
-        c._get_baremetal_nodes("context").AndReturn(hosts)
+        self.mox.StubOutWithMock(bm_driver, '_get_baremetal_nodes')
+        bm_driver._get_baremetal_nodes("context").AndReturn(hosts)
         self.mox.ReplayAll()
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.mox.VerifyAll()
         self.assertEqual(result['id'], 3)
 
@@ -272,35 +304,35 @@ class FindHostTestCase(test.TestCase):
         h2 = utils.new_bm_node(id=2, memory_mb=2048, service_host="host1")
         h3 = utils.new_bm_node(id=3, memory_mb=1024, service_host="host1")
         hosts = [h1, h2, h3]
-        self.stubs.Set(c, '_get_baremetal_nodes', lambda self: hosts)
+        self.stubs.Set(bm_driver, '_get_baremetal_nodes', lambda self: hosts)
         inst = {'vcpus': 1}
 
         inst['memory_mb'] = 1
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 1)
 
         inst['memory_mb'] = 512
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 1)
 
         inst['memory_mb'] = 513
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 3)
 
         inst['memory_mb'] = 1024
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 3)
 
         inst['memory_mb'] = 1025
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 2)
 
         inst['memory_mb'] = 2048
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 2)
 
         inst['memory_mb'] = 2049
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertTrue(result is None)
 
     def test_find_suitable_baremetal_node_about_cpu(self):
@@ -311,21 +343,21 @@ class FindHostTestCase(test.TestCase):
         n3 = utils.new_bm_node(id=3, cpus=3, memory_mb=512,
                                service_host="host1")
         nodes = [n1, n2, n3]
-        self.stubs.Set(c, '_get_baremetal_nodes', lambda self: nodes)
+        self.stubs.Set(bm_driver, '_get_baremetal_nodes', lambda self: nodes)
         inst = {'memory_mb': 512}
 
         inst['vcpus'] = 1
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 1)
 
         inst['vcpus'] = 2
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 2)
 
         inst['vcpus'] = 3
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertEqual(result['id'], 3)
 
         inst['vcpus'] = 4
-        result = c._find_suitable_baremetal_node("context", inst)
+        result = bm_driver._find_suitable_baremetal_node("context", inst)
         self.assertTrue(result is None)
