77c9de3512ba1b99aa471117c1995a153ecf5306
Eliot J. Kang <eliot@savinetwork.ca>
2013-02-25 19:19:25 -0500

    Add a temperature field to the compute_nodes table

--- nova/db/sqlalchemy/migrate_repo/versions/082_essex.py
+++ nova/db/sqlalchemy/migrate_repo/versions/082_essex.py
@@ -221,6 +221,7 @@ def upgrade(migrate_engine):
         Column('current_workload', Integer),
         Column('running_vms', Integer),
         Column('hypervisor_hostname', String(length=255)),
+        Column('temperature', Text, nullable=False),
         mysql_engine='InnoDB',
         #mysql_charset='utf8'
     )
